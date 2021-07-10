4ab98e3da743d3494537f194257671475ae2a668
BRANGONG <bsingpho@gmail.com>
2021-07-10 12:28:04 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://Brangong:Brangong.123@cluster0.bp5om.mongodb.net/poll?retryWrites=true&w=majority',
+  pusherAppId: '1233558',
+  pusherKey: '811488bd2d039c1c705f',
+  pusherSecret: '3c18603188af39df3104',
+  pusherCluster: 'ap2',
+  pusherEncrypted: true
+};
